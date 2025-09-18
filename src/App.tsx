753f27c259ecd48b7e687c7c45ce2d2664cf5257
minhcu <minhdong1831999@gmail.com>
2025-09-18 21:33:53 +0700

    feat: 1st class

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+import LoginPage from "./modules/login/LoginPage"
+
+function App() {
+  return (
+    <>
+      <LoginPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/modules/login/LoginPage.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function LoginPage() {
+    function getData() {
+        alert("Hello");
+    }
+
+    return <>
+        <Button >Click me</Button>
+    </>;
+}
